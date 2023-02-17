f8241a7117571d4662053dcf861d965c21045c85
AdrianMay200 <adrian.19070041@itsmotul.edu.mx>
2023-02-17 12:37:19 -0600

    actualizado

--- tareaVue/src/App.vue
+++ tareaVue/src/App.vue
@@ -2,20 +2,16 @@
 //import HelloWorld from './components/HelloWorld.vue'
 //import TheWelcome from './components/TheWelcome.vue'
 ///
+import nombre from './components/cabecera.vue'
 </script>
 
 <template>
-
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <nombre />
   </header>
 
   <main>
-    <TheWelcome />
+    <cabecera />
   </main>
 </template>
 
